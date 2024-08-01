6ba70d7df82a272862074454bd36930b28f56346
Emre Iyican <git@iyican.me>
2024-08-02 00:57:09 +0300

    Fix network_address.rs warnings

--- src/network_address.rs
+++ src/network_address.rs
@@ -12,9 +12,9 @@ pub struct NetworkAddress {
 
 impl std::fmt::Display for NetworkAddress {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(
+        writeln!(
             f,
-            "{0}.{1}.{2}.{3}:{4}\n",
+            "{0}.{1}.{2}.{3}:{4}",
             self.a, self.b, self.c, self.d, self.port
         )
     }
@@ -22,7 +22,7 @@ impl std::fmt::Display for NetworkAddress {
 
 impl Hash for NetworkAddress {
     fn hash<H: Hasher>(&self, hasher: &mut H) {
-        let hash = NetworkAddress::get_hash(&self);
+        let hash = Self::get_hash(self);
         hasher.write_u32(hash);
         hasher.finish();
     }
@@ -39,19 +39,20 @@ impl PartialEq for NetworkAddress {
 }
 
 impl NetworkAddress {
-    pub fn test_address() -> Self {
-        let address = NetworkAddress {
+    #[must_use]
+    pub const fn test_address() -> Self {
+        Self {
             a: 127,
             b: 0,
             c: 0,
             d: 1,
             port: 8265,
-        };
-        return address;
+        }
     }
 
-    pub fn broadcast(channel: u8) -> Self {
-        NetworkAddress {
+    #[must_use]
+    pub const fn broadcast(channel: u8) -> Self {
+        Self {
             a: 255,
             b: 255,
             c: 255,
@@ -60,58 +61,62 @@ impl NetworkAddress {
         }
     }
 
-    pub fn localhost(port: u32) -> Self {
-        let address = NetworkAddress {
+    #[must_use]
+    pub const fn localhost(port: u32) -> Self {
+        Self {
             a: 127,
             b: 0,
             c: 0,
             d: 1,
-            port: port,
-        };
-        return address;
+            port,
+        }
     }
 
-    pub fn mock_client_address() -> Self {
-        let address = NetworkAddress {
+    #[must_use]
+    pub const fn mock_client_address() -> Self {
+        Self {
             a: 127,
             b: 0,
             c: 0,
             d: 1,
             port: 4598,
-        };
-        return address;
-    }
-
-    pub fn from_socket_addr(address: SocketAddr) -> NetworkAddress {
-        if let IpAddr::V4(ipv4) = address.ip() {
-            let parts = ipv4.octets();
-            let result = NetworkAddress {
-                a: parts[0] as u16,
-                b: parts[1] as u16,
-                c: parts[2] as u16,
-                d: parts[3] as u16,
-                port: address.port() as u32,
-            };
-            return result;
-        } else {
-            return NetworkAddress::default();
         }
     }
 
-    pub fn to_socket_addr(&self) -> SocketAddr {
+    #[must_use]
+    pub fn from_socket_addr(address: SocketAddr) -> Self {
+        let IpAddr::V4(ipv4) = address.ip() else {
+            return Self::default();
+        };
+
+        let parts = ipv4.octets();
+        Self {
+            a: parts[0] as u16,
+            b: parts[1] as u16,
+            c: parts[2] as u16,
+            d: parts[3] as u16,
+            port: address.port() as u32,
+        }
+    }
+
+    #[must_use]
+    pub const fn to_socket_addr(&self) -> SocketAddr {
         let ip = Ipv4Addr::new(self.a as u8, self.b as u8, self.c as u8, self.d as u8);
-        return SocketAddr::new(IpAddr::V4(ip), self.port as u16);
+
+        SocketAddr::new(IpAddr::V4(ip), self.port as u16)
     }
 
+    #[must_use]
     pub fn is_default(&self) -> bool {
-        return NetworkAddress::default() == *self;
+        Self::default() == *self
     }
 
-    pub fn is_broadcast(&self) -> bool {
+    #[must_use]
+    pub const fn is_broadcast(&self) -> bool {
         self.a == 255 && self.b == 255 && self.c == 255 && self.d == 255
     }
 
-    pub fn copy_from(&mut self, other: NetworkAddress) {
+    pub fn copy_from(&mut self, other: Self) {
         self.a = other.a;
         self.b = other.b;
         self.c = other.c;
@@ -119,19 +124,15 @@ impl NetworkAddress {
         self.port = other.port;
     }
 
-    pub fn get_hash(&self) -> u32 {
+    #[must_use]
+    pub const fn get_hash(&self) -> u32 {
         let mut hash: u32 = 17;
         hash = hash.wrapping_mul(23).wrapping_add(self.a as u32);
         hash = hash.wrapping_mul(23).wrapping_add(self.b as u32);
         hash = hash.wrapping_mul(23).wrapping_add(self.c as u32);
         hash = hash.wrapping_mul(23).wrapping_add(self.d as u32);
-        hash = hash.wrapping_mul(23).wrapping_add(self.port as u32);
+        hash = hash.wrapping_mul(23).wrapping_add(self.port);
 
-        /* hash = hash * 23 + self.a as u32;
-        hash = hash * 23 + self.b as u32;
-        hash = hash * 23 + self.c as u32;
-        hash = hash * 23 + self.d as u32;
-        hash = hash * 23 + self.port as u32; */
-        return hash;
+        hash
     }
 }
