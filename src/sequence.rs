9c8c1e8edbe8a0eb83a319608151e563166082b1
Emre Iyican <git@iyican.me>
2024-08-01 20:59:06 +0300

    Fix sequence.rs warnings

--- src/sequence.rs
+++ src/sequence.rs
@@ -1,34 +1,36 @@
 pub struct Sequence {}
 
 impl Sequence {
-    pub fn is_greater_then(s1: u16, s2: u16) -> bool {
-        return ((s1 > s2) && (s1 - s2 <= 32768)) || ((s1 < s2) && (s2 - s1 > 32768));
+    #[must_use]
+    pub const fn is_greater_then(s1: u16, s2: u16) -> bool {
+        ((s1 > s2) && (s1 - s2 <= 32768)) || ((s1 < s2) && (s2 - s1 > 32768))
     }
 
-    pub fn is_less_than(s1: u16, s2: u16) -> bool {
-        return Sequence::is_greater_then(s2, s1);
+    #[must_use]
+    pub const fn is_less_than(s1: u16, s2: u16) -> bool {
+        Self::is_greater_then(s2, s1)
     }
 
-    pub fn is_equal_to_or_less_than(s1: u16, s2: u16) -> bool {
-        if s1 == s2 {
-            return true;
-        }
-        return Sequence::is_greater_then(s2, s1);
+    #[must_use]
+    pub const fn is_equal_to_or_less_than(s1: u16, s2: u16) -> bool {
+        s1 == s2 || Self::is_greater_then(s2, s1)
     }
 
-    pub fn next_sequence(sequence: u16) -> u16 {
-        if sequence >= std::u16::MAX - 1 {
-            return 0;
+    #[must_use]
+    pub const fn next_sequence(sequence: u16) -> u16 {
+        if sequence >= u16::MAX - 1 {
+            0
         } else {
-            return sequence + 1;
+            sequence + 1
         }
     }
 
-    pub fn previous_sequence(sequence: u16) -> u16 {
+    #[must_use]
+    pub const fn previous_sequence(sequence: u16) -> u16 {
         if sequence == 0 {
-            return std::u16::MAX - 1;
+            u16::MAX - 1
         } else {
-            return sequence - 1;
+            sequence - 1
         }
     }
 }
